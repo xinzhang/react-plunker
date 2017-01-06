7812bb0950cc63418e2880162b2ef64ae06c776b
Xin Zhang <Xin.Zhang@ausmaq.com.au>
2017-01-06 16:27:45 +1100

    module 2 -data

--- module1/script.jsx
+++ module1/script.jsx
@@ -1,49 +1,71 @@
-/** @jsx React.DOM */
+// Code goes here
+var Card = React.createClass({
 
-var Button = React.createClass({
-  
-  localHandleClick: function() {
-    this.props.localHandleClick(this.props.increment);
+  getInitialState: function() {
+    return {};
   },
-  
-  render : function() {
-    return (
-        <button onClick={this.localHandleClick}>increment {this.props.increment}</button>
-    )
-  }
-});
 
-var Result = React.createClass({
+  componentDidMount: function() {
+    var component = this;
+    $.get("https://api.gethub.com/users/" + this.props.login, function(data) {
+      component.setState(data);
+    })
+  },
+
   render: function() {
     return (
-      <div>{this.props.localCounter}</div>
-    )
+      <div>
+        <img src="{this.state.avatar_url}" width="80" />
+        <h3>{this.state.name} - {this.props.login}</h3>
+        <hr />
+      </div>
+    );
+  }
+
+});
+
+var Form = React.createClass({
+  handleSubmit : function(e) {
+    e.preventDefault();
+    var loginInput = ReactDOM.findDOMNode(this.refs.login)
+    this.props.addCard(loginInput.value);
+    loginInput.value = '';
+  },
+
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="github logins" ref="login" />
+        <button>Add</button>
+      </form>
+    );
   }
 })
 
 var Main = React.createClass({
-
   getInitialState: function() {
-    return {counter:0}
+    return {logins:[]};
   },
-  
-  handleClick: function(inc) {
-    this.setState({counter: this.state.counter + inc});
+
+  addCard:function(loginToAdd) {
+    this.setState({ logins: this.state.logins.concat(loginToAdd)});
   },
-  
+
   render: function() {
+     var cards = this.state.logins.map(function(login) {
+       return (<Card login={login} />);
+     });
+
     return (
-        <div>
-          <Button localHandleClick={this.handleClick} increment={1} />
-          <Button localHandleClick={this.handleClick} increment={5} />
-          <Button localHandleClick={this.handleClick} increment={10} />
-          <Result localCounter={this.state.counter} />
-        </div>
+      <div>
+        <Form addCard={this.addCard}/>
+        {cards}
+      </div>
     )
   }
-})
+});
 
-React.render(
+ReactDOM.render(
   <Main />,
-  document.getElementById('main')
+  document.getElementById("main")
 )
--- module2/script.jsx
+++ module2/script.jsx
@@ -16,21 +16,43 @@ var Card = React.createClass({
     return (
       <div>
         <img src="{this.state.avatar_url}" width="80" />
-        <h3>{this.state.name}</h3>
+        <h3>{this.state.name} - {this.props.login}</h3>
         <hr />
       </div>
     );
   }
-  
+
 });
 
+var Form = React.createClass({
+  handleSubmit : function() {
 
-var Main = React.createClass({
+  },
+
   render: function() {
     return (
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="github logins" />
+        <button>Add</button>
+      </form>
+    );
+  }
+})
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return {logins:[]};
+  },
+
+  render: function() {
+     var cards = this.state.logins.map(function(login) {
+       return (<Card login={login} />);
+     });
+
+    return (
       <div>
-        <Card login="petehunt" />
-        <Card login="spicy" />
+        <Form />
+        {cards}
       </div>
     )
   }
